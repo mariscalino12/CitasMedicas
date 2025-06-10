ca40e70045a6f7102d679dc55f85251e2702bff7
AlexHB <sirkrav18@gmail.com>
2025-06-09 21:45:16 -0500

    Proyecto Subido

---
+++ src/main/java/com/elp/SRCM/SrcmApplication.java
@@ -0,0 +1,13 @@
+package com.elp.SRCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elp/SRCM/SrcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elp.SRCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
